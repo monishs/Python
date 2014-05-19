b1db32afecc20933bff8f23238cffb52f4dbb75f
monishs <monishs@gmail.com>
2014-05-19 15:16:52 -0700

    Initial Commit

---
+++ MemoryGame.py
@@ -0,0 +1,116 @@
+# implementation of card game - Memory
+
+import simplegui
+import random
+
+WIDTH = 800
+HEIGHT = 100
+CARDWIDTH = 50
+NUMBERS = 8
+NUMBER_SIZE = 20
+
+cardlist = []
+cardpos = []
+exposed = []
+
+
+# helper function to initialize globals
+def create_list_of_random_numbers(numbers):
+    numberlist = range(numbers)
+    random.shuffle(numberlist)
+    
+    print "Number list is:",numberlist
+    return numberlist
+
+
+def new_game():
+    global cardlist, cardpos, exposed, CLICKS, TURNS
+    
+    CLICKS = 0
+    TURNS = 0
+    label.set_text("Turns = " + str(TURNS))
+    
+    # Concatenate the two separate random number lists of range 8 to make it 16 numbers
+    cardlist =  create_list_of_random_numbers(NUMBERS) + create_list_of_random_numbers(NUMBERS)
+    random.shuffle(cardlist)  #Shuffles the 16 numbers inside the list
+    print "Shuffled Cardlist is:", cardlist
+    
+    # Center the Card number position in the CARDWIDTH size 
+    cardpos=[(CARDWIDTH/2)*(2*index+1) for index in range(len(cardlist))]
+    exposed=["F" for index in range(len(cardlist))]
+    
+    print "Card number positions are:", cardpos,
+    #print cardpos[1]
+    #print "\n"
+   
+     
+# define event handlers
+def mouseclick(pos):
+   global click_pos, CLICKS, TURNS, card, card1, card2
+    
+   click_pos = list(pos)
+    
+    # add game state logic here
+   card = click_pos[0]/CARDWIDTH
+   
+   if( exposed[card] == "F" ):
+        exposed[card] = "T"
+        if CLICKS == 0:
+            card1 = card
+            CLICKS = 1
+            #print "Card1 index", card1
+        elif CLICKS == 1:
+            card2 = card
+            #print "Card2 index", card2
+            CLICKS = 2	
+            TURNS += 1
+        else:
+            #print "Card3 index", card
+            CLICKS = 3
+ 
+        
+   #print "Exposed list", exposed
+   print "\nClicks is", CLICKS
+   label.set_text("Turns = " + str(TURNS)) 
+    
+                        
+# cards are logically 50x100 pixels in size    
+def draw(canvas):
+    global CLICKS, card1
+    
+    for index in range(NUMBERS*2):
+        if( exposed[index] == "F" ):
+            #canvas.draw_polyline([(0, 0), (25, 0), (25, HEIGHT)],CARDWIDTH, 'GREEN')
+            #canvas.draw_polyline([(50, 0), (75, 0), (75, HEIGHT)],CARDWIDTH, 'GREEN')
+            canvas.draw_polyline([((CARDWIDTH/2)*(2*index+1)-CARDWIDTH/2, 0), ((CARDWIDTH/2)*(2*index+1), 0), ((CARDWIDTH/2)*(2*index+1), HEIGHT)],CARDWIDTH, 'GREEN')
+        elif( exposed[index] == "T" ):   
+            canvas.draw_text(str(cardlist[index]), [cardpos[index],HEIGHT/2], NUMBER_SIZE, "White")
+            
+            if( CLICKS == 3 ):
+                if( cardlist[card2] != cardlist[card1] ):   # because comparison is at the 3rd open card
+                    exposed[card2] = exposed[card1] = "F" 
+                    print "NOW CLICKS is", CLICKS 
+                    
+                print "NOW Card1, Card2, Card3 indexes are:", card1, card2, card
+                CLICKS = 1       # Its 3 clicks so reset back to 1 click for 1 Open card
+                card1 = card   
+                print "..AFTER Reset to 1 click, Card1, Card2, Card3 indexes are:", card1, card2, card
+                   
+        canvas.draw_line((CARDWIDTH*(index+1), 0), (CARDWIDTH*(index+1), HEIGHT), 1, 'WHITE')
+
+        
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", WIDTH, HEIGHT)
+frame.add_button("Reset", new_game)
+label = frame.add_label("Turns = 0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+# get things rolling
+new_game()
+frame.start()
+
+
+# Always remember to review the grading rubric
---
+++ StopWatch.py
@@ -0,0 +1,89 @@
+# template for "Stopwatch: The Game"
+
+import simplegui
+
+# define global variables
+Time = 0
+Interval = 100
+Ticks = 0
+Wins = 0
+Attempts = 0
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+def format(t):
+    global Time
+    
+    val = str(t)
+    if( int(val) >= 0 ):
+        Mins = int(val[0:])/600
+        Secs = (float(val[0:])/600 - Mins) * 60
+        #print "Mins & Secs", Mins, Secs
+    if ( len(str(Secs)) == 3):    ## eg: 0.1
+        Secs = "0" + str(Secs)
+        #print Secs
+    else:                         ## eg: 59.9
+        Secs = str(Secs)    
+        #print Secs
+    Time = str(Mins) + ":" + Secs
+    return Time
+    
+# define event handlers for buttons; "Start", "Stop", "Reset"
+def start():
+     timer.start()
+
+def stop():
+      global Time, Wins, Attempts
+      
+      if( timer.is_running() ):
+            timer.stop()  
+            
+            if( Time[4:6] == ".0" ):
+                Wins = Wins + 1
+                Attempts = Attempts + 1
+            
+            else:
+                Attempts = Attempts + 1
+    
+      score_handler()      
+            
+def reset():
+     global Ticks, Wins, Attempts
+     Ticks = 0
+     Wins = 0
+     Attempts = 0
+     
+     timer.stop()
+      
+        
+# define event handler for timer with 0.1 sec interval
+def timer_handler():
+    global Ticks
+    Ticks = Ticks + 1
+    
+# define Score handler 
+def score_handler():
+    DisplayScore = str(Wins) + "/" + str(Attempts)
+    return DisplayScore
+
+# define draw handler
+def draw_handler(canvas):
+    canvas.draw_text(format(Ticks), [50,100], 36, "White")
+    canvas.draw_text(score_handler(), [140,20], 25, "Green")
+    
+# create frame
+frame = simplegui.create_frame("StopWatch", 200, 200)
+
+# register event handlers
+frame.add_button("Start", start, 100)
+frame.add_button("Stop", stop, 100)
+frame.add_button("Reset", reset, 100)
+frame.set_draw_handler(draw_handler)
+timer = simplegui.create_timer(Interval, timer_handler)
+
+
+# start frame
+frame.start()
+
+
+# Please remember to review the grading rubric
